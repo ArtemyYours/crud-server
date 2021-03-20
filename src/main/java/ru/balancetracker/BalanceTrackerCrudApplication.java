aa6e8b2a939aa10dfc8537aa3e9029e7d6bb9868
Artem Kopilov <artem.kopilov@advlab.io>
2021-03-20 13:39:35 +0300

    [CU-e8xcb8] docker.compose + main class added [dev, feature]

---
+++ src/main/java/ru/balancetracker/BalanceTrackerCrudApplication.java
@@ -0,0 +1,15 @@
+package ru.balancetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class BalanceTrackerCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BalanceTrackerCrudApplication.class, args);
+    }
+
+}
