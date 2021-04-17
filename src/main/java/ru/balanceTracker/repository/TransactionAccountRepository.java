a3da2d92955a4214caac6689c4009504bf548595
Artem Kopilov <artem.kopilov@advlab.io>
2021-04-17 17:11:45 +0300

    [CU-e8xcb8] repo created [dev, feature]

--- src/main/java/ru/balanceTracker/repository/TransactionAccountRepository.java
+++ src/main/java/ru/balanceTracker/repository/TransactionAccountRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import ru.balanceTracker.model.TransactionAccount;
 
 public interface TransactionAccountRepository extends JpaRepository<TransactionAccount, Long> {
+
 }
--- src/main/java/ru/balanceTracker/repository/TransactionRepository.java
+++ src/main/java/ru/balanceTracker/repository/TransactionRepository.java
@@ -4,4 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import ru.balanceTracker.model.Transaction;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+
+
 }
