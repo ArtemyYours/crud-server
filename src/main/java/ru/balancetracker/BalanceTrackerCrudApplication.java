6cd10f55c4e260dbdb4d39282db31d13a00defcf
akopilov <artemykopilov@gmail.com>
2021-06-14 17:44:01 +0300

    CICD configured

--- src/main/java/ru/balancetracker/BalanceTrackerCrudApplication.java
+++ src/main/java/ru/balancetracker/BalanceTrackerCrudApplication.java
@@ -2,10 +2,15 @@ package ru.balancetracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import ru.balancetracker.security.KeycloakClientCredentials;
+import ru.balancetracker.security.KeycloakProperties;
 
 @SpringBootApplication
 @EnableJpaAuditing
+
+@EnableConfigurationProperties({KeycloakProperties.class, KeycloakClientCredentials.class})
 public class BalanceTrackerCrudApplication {
 
     public static void main(String[] args) {
