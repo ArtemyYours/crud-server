ca5dfb913f25e92e3946b994a1da4f2e0a0d4f63
akopilov <artemykopilov@gmail.com>
2021-06-25 09:32:17 +0300

    [CU-e8xcb8] new message codes to reflect transaction account type choice and trnsaction accounnt does not exist implementation [dev, feature]

--- src/main/java/ru/balancetracker/exceptions/MessageCode.java
+++ src/main/java/ru/balancetracker/exceptions/MessageCode.java
@@ -9,6 +9,9 @@ import org.springframework.http.HttpStatus;
 public enum MessageCode {
     USER_DOESNT_HAVE_ACCESS_TO_TRANSACTION("balance-tracker.crud.user-doesn't-have-access-to-transaction"),
     USER_DOESNT_HAVE_ACCESS_TO_TRANSACTION_ACCOUNT("balance-tracker.crud.user-doesn't-have-access-to-transaction-account"),
+    THIS_TRANSACTION_ACCOUNT_DOES_NOT_EXIST("balance-tracker.crud.transaction-account-does-not-exist"),
+    INCOME_CANNOT_BE_DESTINATION("balance-tracker.crud.transaction-account-type-income-cannot-be-used-as-destination"),
+    OUTCOME_CANNOT_BE_SOURCE("balance-tracker.crud.transaction-account-type-outcome-cannot-be-used-as-source"),
 
     ;
 
